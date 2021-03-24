c19e288713080932715ec05aa3b53173c6a4aa19
gabrielb77 <gabrielb77@gmail.com>
2021-03-24 08:49:21 -0300

    newmod

--- chgate.py
+++ chgate.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from pythonping import ping
+#from pythonping import ping
 from pyroute2 import IPRoute
+import ping3
 
 
 def main():
@@ -9,21 +10,24 @@ def main():
 #    ping('192.168.0.1', verbose=True, size=248, count=3)
 #    ping('www.google.com', verbose=True, size=248, count=3)
 
-  response = ping("192.168.0.1", size=248, count=1)
-  if response is response:
-    print("Dio timeout")
+#  response = ping("192.168.0.1", size=248, count=1)
+#  if response is response:
+#    print("Dio timeout")
 
-  print(response.rtt)
+#  print(response)
 
 
-  ipr = IPRoute()
+#  ipr = IPRoute()
 
-  print(ipr.get_addr(family=2))
+#  print(ipr.get_addr(family=2))
 
-  print(ipr.get_default_routes(family=0, table=254))
+#  print(ipr.get_default_routes(family=0, table=254))
 
-  ipr.route("del", dst="default", gateway="192.168.0.40")
-  ipr.route("add", dst="default", gateway="192.168.0.1")
+#  ipr.route("del", dst="default", gateway="192.168.0.40")
+#  ipr.route("add", dst="default", gateway="192.168.0.1")
+  print(ping3.ping("192.168.0.1"))
+  resp = ping3.ping("192.168.0.1")
+
 
 
 if __name__ == "__main__":
